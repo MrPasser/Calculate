a1fdbc7b26257e4a89af3e48d04fef434a28de86
Svyatoslav <SvGoldKr@gmail.com>
2016-08-09 18:22:06 +0300

    Initial Commit

---
+++ Calculate/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Calculate
+//
+//  Created by Sviatoslav Krasin on 09.08.16.
+//  Copyright © 2016 Sviatoslav Krasin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
